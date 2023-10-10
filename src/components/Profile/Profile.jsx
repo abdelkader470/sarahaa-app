88ff57f4a0d852ba9d062ce792ddc69e79c6fbd1
Abdelkader Elsisy <66458422+abdelkader470@users.noreply.github.com>
2023-10-10 23:54:41 +0300

    fixed errors

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,20 +1,21 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Profile.module.css";
 import avatar from "../../images/avatar.png";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import jwtDecode from "jwt-decode";
 import { useQuery } from "react-query";
+
 function Profile() {
   const [userId, setUserId] = useState("");
   const {
-    data: allMessages,
-    isLoading,
+    data: messages,
     isError,
-  } = useQuery("userMessages", fetchUserMessages);
+    isLoading,
+  } = useQuery("messages", fetchMessages);
 
-  async function fetchUserMessages() {
-    const response = await fetch(
+  async function fetchMessages() {
+    const response = await axios.get(
       "https://sara7aiti.onrender.com/api/v1/message",
       {
         headers: {
@@ -22,37 +23,32 @@ function Profile() {
         },
       }
     );
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  }
-
-  function getUserId() {
-    let decoded = jwtDecode(localStorage.getItem("userToken"));
-    console.log(decoded);
-    setUserId(decoded.id);
+    return response.data.allMessages;
   }
 
   useEffect(() => {
-    getUserId();
+    const decoded = jwtDecode(localStorage.getItem("userToken"));
+    setUserId(decoded.id);
   }, []);
+
   return (
     <>
       <div className="container text-center py-5 my-5 text-center">
         <div className="card pt-5">
-          <a href data-toggle="modal" data-target="#profile">
-            <img src={avatar} className="avatar " alt="" />
+          <a href="#" data-toggle="modal" data-target="#profile">
+            <img src={avatar} className="avatar" alt="" />
           </a>
           <h3 className="py-2">Abdelkader Elsisy</h3>
-          <Link
-            data-toggle="modal"
-            data-target="#share"
-            className="btn btn-default-outline share "
-            to={"/message/" + userId}
-          >
-            <i className="fas fa-share-alt" /> Share Profile
-          </Link>
+          {userId && (
+            <Link
+              data-toggle="modal"
+              data-target="#share"
+              className="btn btn-default-outline share"
+              to={`/message/${userId}`}
+            >
+              <i className="fas fa-share-alt" /> Share Profile
+            </Link>
+          )}
         </div>
       </div>
 
@@ -70,15 +66,15 @@ function Profile() {
                 <p>Error fetching messages</p>
               </div>
             </div>
-          ) : allMessages && allMessages.length === 0 ? (
+          ) : messages && messages.length === 0 ? (
             <div className="col-md-12">
               <div className="card py-5">
                 <p>You don't have any messages... </p>
               </div>
             </div>
           ) : (
-            allMessages.map((ele) => (
-              <div key={ele.id} className="col-md-12">
+            messages.map((ele) => (
+              <div key={ele._id} className="col-md-12">
                 <div className="card py-5">
                   <p>{ele.messageContent}</p>
                 </div>
